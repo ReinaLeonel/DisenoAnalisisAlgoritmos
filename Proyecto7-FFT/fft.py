8bb1924863a2cc7c94bff4638294b407a9383da3
Reina J Leonel <reina.zirtaeb@gmail.com>
2025-01-13 12:09:38 -0600

    Tranformada funcionando, lectura de audio en testeo

--- Proyecto7-FFT/fft.py
+++ Proyecto7-FFT/fft.py
@@ -2,10 +2,12 @@
 
 # Librerias
 import math
+import wave
 
 # Constantes
 PI = math.pi
 
+# Funcion que realiza la FFT
 def fft(n, a):
     """
     Funcion que realiza la FFT
@@ -13,12 +15,8 @@ def fft(n, a):
     :param a: Arreglo de numeros
     """
     # Caso base
-    if n <= 1:
-        return a
-
-    #inicializando variables
-    even = []
-    odd = []
+    if n == 1:
+        return [a[0]]
     
     # Se divide el arreglo, en pares e impares
     even = fft(n//2, a[0::2]) # Pares
@@ -38,10 +36,10 @@ def fft(n, a):
     #     # w[k] = math.cos(2*PI*k/n), 1j*math.sin(2*PI*k/n)
 
     # Se realiza la FFT
-    for k in range((n//2)-1):
+    for k in range((n//2)):
         # Se calcula la raiz de la unidad
         # w[k] = complex(math.cos(2*PI*k/n), math.sin(2*PI*k/n))
-        w.insert(k, complex(math.cos(2*PI*k/n), math.sin(2*PI*k/n)))
+        w.insert(k, complex(math.cos(-2*PI*k/n), math.sin(-2*PI*k/n)))
 
         # Unimos los pares e impares (Combinar resultados)
         y.insert(k, even[k] + w[k]*odd[k]) # y[k] = even[k] + w[k]*odd[k]
@@ -49,10 +47,41 @@ def fft(n, a):
 
     return y
 
+# Funcion que lee archivo de audio
+def readAudioFile(file):
+    """
+    Funcion que lee un archivo de audio
+    :param file: Archivo de audio
+    """
+    # Se abre el archivo
+    audio = wave.open(file, 'r')
+
+    # Se obtienen los parametros del archivo
+    params = audio.getparams()
+    nchannels, sampwidth, framerate, nframes = params[:4]
+
+    # Se lee el archivo
+    frames = audio.readframes(nframes * nchannels)
+    out = []
+
+    # Se obtienen los valores de los frames
+    for i in range(0, len(frames), 2):
+        low = frames[i]
+        high = frames[i + 1]
+        value = (high * 256) + low # Es 256 porque es 2^8 para hacer un desplazamiento de 8 bits a la izquierda
+        out.append(value)
+
+    return out
+
 # Prueba
 
-a = [1, 2, 3, 4, 5, 6, 7, 8]
-n = len(a)
-b = fft(n, a)
-for B in b:
-    print(B)
+# a = [1, 2, 3, 4]
+# n = len(a)
+# b = fft(n, a)
+# print("---------------------------------------------")
+# for B in b:
+#     print(B)
+
+# Se lee el archivo de audio
+audio = readAudioFile('./Sonidos/acoustic.wav')
+print(audio)
